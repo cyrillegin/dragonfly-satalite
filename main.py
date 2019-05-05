d635716e28067b3a378e81b77a05ae0c36108748
Cyrille Gindreau <cyrillegin@gmail.com>
2019-05-04 20:14:51 -0600

    setting up temp sensor

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from config import baseStation, sensors
+from sensors.gpio import readTemp, setup
 
 if __name__ == "__main__":
     print('hello world')
     print(baseStation)
     for i in sensors:
         print (i['name'])
+        if i['name'] == 'gpio':
+            file = setup()
+            readTemp(file)
--- sensors/gpio.py
+++ sensors/gpio.py
@@ -1,48 +1,59 @@
 import time
 import logging
+import os
+import glob
 
 logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', level=logging.INFO)
 
 
-def setup(params):
-    pass
+def setup():
+    # Made sure `dtoverlay=w1-gpio` is in /boot/config.txt (requires reboot)
+    # run:
+        # sudo modprobe w1-gpio
+        # sudo modprobe w1-therm
+        # cd /sys/bus/w1/devices
+        # ls
+    # There should be a folder starting with 28-
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+
+    deviceFolder = glob.glob('/sys/bus/w1/devices/28*')[0]
+    deviceFile = deviceFolder + '/w1_slave'
+    return deviceFile
 
 
-def readTemperature(deviceLocation):
-    f = open(deviceLocation, 'r')
+# A function that reads the sensors data
+def readTempRaw(deviceFile):
+    f = open(deviceFile, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 
-def GetValues(params):
-    deviceLocation = "/sys/bus/w1/devices/{}/w1_slave".format(params['meta'])
+# Convert the value of the sensor into a temperature
+def readTemp(deviceFile):
+    lines = readTempRaw(deviceFile)
 
-    lines = readTemperature(deviceLocation)
+    # While the first line does not contain 'YES', wait for 0.2s
+    # and then read the device file again.
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = readTemperature(deviceLocation)
+        lines = readTempRaw()
 
-    temp_output = lines[1].find('t=')
+    # Look for the position of the '=' in the second line of the
+    # device file.
+    equals_pos = lines[1].find('t=')
 
-    if temp_output != -1:
-        temp_string = lines[1].strip()[temp_output + 2:]
+    # If the '=' is found, convert the rest of the line after the
+    # '=' into degrees Celsius, then degrees Fahrenheit
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-
-        logging.debug('Temperature is currently: {}'.format(temp_f))
-        newReading = {
-            'sensor': {
-                'uuid': params['uuid'],
-                'poller': 'gpioPoller'
-            },
-            'reading': {
-                'timestamp': time.time() * 1000,
-                'value': temp_f,
-            }
-        }
-        return newReading
+        return temp_f
 
 
 if __name__ == '__main__':
-    GetValues({'pin': '4', 'meta': '28-0516a43668ff', 'uuid': 'test'})
+    while True:
+        print(readTemp())
+        time.sleep(1)
